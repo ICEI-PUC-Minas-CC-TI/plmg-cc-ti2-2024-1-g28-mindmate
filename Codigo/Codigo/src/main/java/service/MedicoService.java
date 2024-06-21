37691666a5e26a7b84f3bb38576d6d277bc8b7d2
vitaoam <130377853+vitaoam@users.noreply.github.com>
2024-06-21 10:35:04 -0300

    Medico Service sem comentario

--- Codigo/Codigo/src/main/java/service/MedicoService.java
+++ Codigo/Codigo/src/main/java/service/MedicoService.java
@@ -52,41 +52,4 @@ public class MedicoService {
 			return "{\"message\": \"Despesa não encontrada\"}";
 		}
 	}
-
-	// public Object update(Request request, Response response) {
-	// 	try{
-	// 		String json = request.body();
-	// 		Gson gson = new Gson();
-	// 		String id = request.params(":id");
-	
-	// 		Despesa despesa = gson.fromJson(json, Despesa.class);
-	
-	// 		if (DespesaDAO.update(despesa, Integer.parseInt(id)) == true) {
-	// 			response.status(200);
-	// 			return new Gson().toJson(despesa);
-	// 		} else {
-	// 			response.status(404);
-	// 			return "{\"message\": \"Despesa não encontrada\"}";
-	// 		}
-	// 	}catch(NullPointerException e){
-	// 		response.status(500);
-	// 		return "{\"message\": \"Ocorreu um erro no servidor\"}";
-	// 	}
-	// }
-
-	// public Object delete(Request request, Response response) {
-	// 	String id = request.params(":id");
-
-	// 	Despesa despesa = DespesaDAO.getAll(Integer.parseInt(id));
-
-	// 	if (despesa != null) {
-	// 		DespesaDAO.delete(despesa.getId());
-	// 		response.status(200);
-	// 		return "{\"message\": \"Despesa excluída com sucesso\"}";
-	// 	} else {
-	// 		response.status(404);
-	// 		return "{\"message\": \"Despesa não encontrada\"}";
-	// 	}
-	// }
-
 }
